7942a91dc201547d92243d4fe6117385966527c1
Gabriel Barbosa Pereira <superxl8gbpaa@outlook.com>
2021-02-23 12:29:36 -0300

    Terminando o projeto

--- main.cpp
+++ main.cpp
@@ -5,20 +5,34 @@ void line(char n = '-', int v = 50);
 
 void Register();
 
+int login();
+
 int main(){
-    int choice;
+    int choice, r;
     while(true){
         std :: cout << "Login and register system\nRegister[1]\nLogin[2]\nYour choice: ";
         std :: cin >> choice;
         if(choice == 1){
             line();
             Register();
+            line();
         }
         else if(choice == 2){
-
+            line();
+            r = login();
+            if(r == 1){
+                std :: cout << "Successfully login\n";
+                break;
+            }
+            else{
+                std :: cout << "Username or password wrongs\n";
+                line();
+            }
+            
         }
         else{
             std :: cout << "ERROR, try again\n";
+            line();
         }
     }
     system("pause");
@@ -45,6 +59,26 @@ void Register(){
     }
     arquiveR.close();
     arquiveW.close();
+}
+
+int login(){
+    std :: string user, password, test, line;
+    std :: cout << "Username: ";
+    std :: cin >> user;
+    std :: cout << "Password: ";
+    std :: cin >> password;
+    test = user + " | " + password;
+    std :: ifstream arquiveR("database.txt");
+    if(arquiveR.is_open()){
+        while(getline(arquiveR, line)){
+            if(test == line){
+                arquiveR.close();
+                return 1;
+            }
+        }
+    }
+    arquiveR.close();
+    return 0;
 }
 
 void line(char n, int v){
